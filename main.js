db3b584da4e2014c53bb21da5532ac44b4bdf0a7
luxihuang <Luxi.Huang@gmail.com>
2017-06-11 19:57:06 -0400

    continued to work on HW, June 11th

--- main.js
+++ main.js
@@ -1,27 +1,60 @@
-//addeventlistener play, stop, and load songs.
-	
-	var video = document.getElementById("myVideo");
-	var playVideo = document.getElementById("playButton");
-	var pauseVideo = document.getElementById("pauseButton");
-	var nextVideo = document.getElementById("nextButton");
-	var previousVideo = document.getElementById("previousButton");
+var JukeBox = {
+	songs = [];
+	activeSong: null; //this will be the song object later
+	dom: {};
 
-	playVideo.onclick = function(){
-		video.play();
+
+	init: function(){
+		var play = document.getElementById("js-playButton"),
+		var stop = document.getElementById("js-stopButton"),
+		var next: document.getElementById("js-nextButton"),
+		var previous: document.getElementById("js-previousButton"),
+			}
+		this.bind(); //calling the bind function
+		}
+
+	bind: function(){
+		this.dom.play.addEventListener("click", this.play);
+		this.dom.stop.addEventListener("click", this.stop);
+		this.dom.next.addEventListener("click", this.next);
+		this.dom.mute.addEventListener("click", function(){
+			this.setVolumne(0);
+		}
 	}
-	
-	pauseVideo.onclick = function(){
-		video.pause();
+
+	play: function(song){
+		console.log("Jukebox is playing");
+		this.activeSong = song;
 	}
 
-	// nextVideo.onlick = function(){
-	//	video. ;
-	// }
+	pause: function(){
+		console.log("Jukebox is pausing");
+	}
 
+	stop: function(){
+		console.log("Jukebox is stoping");
+	}
 
+	change: function(song){
+		console.log("Jukebox is changing");
+	}
 
-var jukeBox = function(videoName){
-	var videoNames = ['bunny.mp4', 'lava.mp4', 'partly_cloudly.mp4'];
-	this.videoName = videoNames[videoName]; //user passes in the video
+	next: function(){
+		console.log("Jukebox is skipping to the next song");
+	}
+
+	shuffle: function(){
+		console.log("Jukebox is shuffling");
+	}
+
+//volume should be a $ number
+	setVolume: function(volumeLevel){
+		console.log("volumne level is this");
+	}
 }
 
+//once document is ready, call Jukebox init function
+document.addEventListener("DOMContentLoaded", function(){
+  // Handler when the DOM is fully loaded
+  Jukebox.init();
+});
