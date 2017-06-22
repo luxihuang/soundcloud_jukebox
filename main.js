52a16e759b2c9a93b28eb445c643a57171906848
luxihuang <Luxi.Huang@gmail.com>
2017-06-21 21:09:42 -0400

    first commit for jukebox using soundcloud's api

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
+SC.initialize({
+  client_id: 'fd4e76fc67798bfa742089ed619084a6'
+});
+
+
 var JukeBox = {
-    songs:['bunny.mp4', 'partly_cloudly.mp4', 'lava.mp4'],
+    songs:[],
     currentSongId: 0,
     previousTime: 0,
 
     init: function() {
         var _this = this;  //making the object available to the methods
+        this.searchInput = document.getElementById("js-search-input");
         this.video = document.getElementById("js-Video");
         this.playButton = document.getElementById("js-playButton");
         this.stopButton = document.getElementById("js-stopButton");
@@ -12,36 +18,60 @@ var JukeBox = {
         this.previousButton = document.getElementById("js-previousButton");
    
         this.playButton.addEventListener("click", function() {
-            _this.load(_this.currentSongId);
-            _this.play();
+            _this.play(_this.currentSongId);
         });  
 
-        this.stopButton.addEventListener("click", function() {
-            _this.stop(_this.currentSongId);
+        // this.stopButton.addEventListener("click", function() {
+        //     _this.stop(_this.currentSongId);
+        // });
+        
+        // this.nextButton.addEventListener("click", function() {
+        //     _this.next(_this.currentSongId);
+        // });
+
+        // this.previousButton.addEventListener("click", function() {
+        //      _this.previous(_this.currentSongId);
+        // }); 
+
+        this.search('Lady Gaga'); //loads a search searching for keyword 'Prince' when you first load
+    },
+
+    // render: function(){
+
+    // },
+
+    search: function(searchTerm){
+        var self = this
+        SC.get('/tracks', {
+            q: searchTerm
+        }).then(function(tracks) {
+            self.songs = tracks.map(function(item){
+                return item.id
+            });
+            console.log(self.songs); //self is only within this scope
         });
         
-        this.nextButton.addEventListener("click", function() {
-            _this.next(_this.currentSongId);
-        });
-
-        this.previousButton.addEventListener("click", function() {
-             _this.previous(_this.currentSongId);
-        }); 
     },
+        
+    stream: function(id){
+        console.log(id);
+        var self = this;  //self is destroyed after the function, but it connects to this
+        SC.stream('/tracks/' + id).then(function(player){
+            self.player = player //makes soundcloud player available for other functions
+            console.log(player)
+            player.play();
+        });    
+    }, 
 
-    load: function(id) { 
-        this.video.src = this.songs[id];
-        this.video.load();
-    },
-
-    play: function() {
-        this.video.play();
-        this.video.currentTime = this.previousTime || 0;
+    play: function(id) {
+        console.log(id);
+        this.stream(id);
     },
   
     stop: function() {
-        this.previousTime = this.video.currentTime;
-        this.video.pause();
+        this.player.pause(); //Soundcloud player
+    //     this.previousTime = this.video.currentTime;
+    //     this.video.pause();
     },
  
     next: function() {     
@@ -50,25 +80,27 @@ var JukeBox = {
         } else {
             this.currentSongId = this.currentSongId + 1;
         }
-        this.load(this.currentSongId);
-        this.play();
+        console.log(currentSongId); 
+        this.player.play(); //same as JukeBox.play()
     },  
 
-    previous: function() {
-        if(this.currentSongId <= 0) {
-            this.currentSongId = 0;
-        } else{
-            this.currentSongId = this.currentSongId - 1;
-    }
+    // previous: function() {
+    //     if(this.currentSongId <= 0) {
+    //         this.currentSongId = 0;
+    //     } else{
+    //         this.currentSongId = this.currentSongId - 1;
+    // }
 }
 
 //once document is ready, call JukeBox init function. Handler when the DOM is fully loaded
-document.addEventListener("DOMContentLoaded", function(){
-    JukeBox.init();
+document.addEventListener("DOMContentLoaded", function(event) {
+  // searchInput.addEventListener('keyup', function(evt){
+  //   search(searchInput.value);
+  // })
+  JukeBox.init();
 });
 
-// not incldued
-// progress bar
-// list of videos
-// sound up and down , mute
-// when you stop the video then play again, the video flashes because it's going all the way back to the currentTime = 0
+
+//for about.html will ahve to type it in
+// run command 'simplehttpserver' when in the specific project folder to do http://localhost:8000/
+
